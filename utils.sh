8fd0c415ce3fb66be59d74ec75c74e73a8e37899
TidalMist <91356643+TidalMist@users.noreply.github.com>
2024-01-29 07:35:03 +0300

    Update utils.sh

--- utils.sh
+++ utils.sh
@@ -284,9 +284,7 @@ get_uptodown_vers() { $HTMLQ --text ".version" <<<"$__UPTODOWN_RESP__"; }
 dl_uptodown() {
 	local uptodown_dlurl=$1 version=$2 output=$3
 	local url
-	if [ -n "$version" ]; then
-		url=$(grep -F "${version}</span>" -B 2 <<<"$__UPTODOWN_RESP__" | head -1 | sed -n 's;.*data-url=".*download\/\(.*\)".*;\1;p') || return 1
-	else url=""; fi
+	url=$(grep -F "${version}</span>" -B 2 <<<"$__UPTODOWN_RESP__" | head -1 | sed -n 's;.*data-url=".*download\/\(.*\)".*;\1;p') || return 1
 	url="https://dw.uptodown.com/dwn/$(req "${uptodown_dlurl}/post-download/${url}" - | sed -n 's;.*class="post-download" data-url="\(.*\)".*;\1;p')" || return 1
 	req "$url" "$output"
 }
@@ -326,8 +324,7 @@ get_archive_pkg_name() { echo "$__ARCHIVE_PKG_NAME__"; }
 patch_apk() {
         local out_aapt2="${TEMP_DIR}/out-aapt2-${app_name_l}-${rv_brand_f}-${version_f}-${arch_f}-${build_mode}.apk"
 	local stock_input=$1 patched_apk=$2 patcher_args=$3 rv_cli_jar=$4 rv_patches_jar=$5
-	declare -r tdir=$(mktemp -d -p $TEMP_DIR)
-        local cmd="java -jar $rv_cli_jar patch $stock_input -r $tdir -p -o $patched_apk -b $rv_patches_jar $patcher_args --options=options.json"
+        local cmd="java -jar $rv_cli_jar patch $stock_input -p -o $patched_apk -b $rv_patches_jar $patcher_args --options=options.json"
         if [ ! "$OS" = Android ]; then cmd+=" --unsigned && ${ANDROID_SDK_ROOT}/build-tools/34.0.0/aapt2 optimize --target-densities xhdpi,xxhdpi $patched_apk -o $out_aapt2 \
 && ${ANDROID_SDK_ROOT}/build-tools/34.0.0/zipalign -p -f 4 $out_aapt2 $patched_apk"; fi
         if [ "$build_mode" = apk ] && [ ! "$OS" = Android ]; then cmd+=" && ${ANDROID_SDK_ROOT}/build-tools/34.0.0/apksigner sign -v --v4-signing-enabled false --cert ./ks.jhc.x509.pem --key ./ks.jhc.pk8 $patched_apk"; fi
@@ -404,8 +401,7 @@ build_rv() {
 		for dl_p in archive apkmirror uptodown apkmonk; do
 			if [ -z "${args[${dl_p}_dlurl]}" ]; then continue; fi
 			pr "Downloading '${table}' from ${dl_p}"
-			if [ "$get_latest_ver" = true ] && [ $dl_p = uptodown ]; then local vver=""; else local vver=$version; fi
-			if ! dl_${dl_p} "${args[${dl_p}_dlurl]}" "$vver" "$stock_apk" "$arch" "${args[dpi]}"; then
+			if ! dl_${dl_p} "${args[${dl_p}_dlurl]}" "$version" "$stock_apk" "$arch" "${args[dpi]}"; then
 				epr "ERROR: Could not download '${table}' from ${dl_p} with version '${version}', arch '${arch}', dpi '${args[dpi]}'"
 				continue
 			fi
